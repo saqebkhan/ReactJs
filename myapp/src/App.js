53195b624a8002628c5aa149ba129d341921444b
saqebkhan <saqebk619@gmail.com>
2021-07-26 13:26:29 +0530

    26july2021

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -21,30 +21,30 @@
 // export default App;
 ///////////////////////for Counter ////////////////////
 
-import './App.css';
-import  { CarouselParent } from './Components/CarouselParent';
-function App () {
-    return(
-        <div>
-            <CarouselParent/>
-        </div>
-    )
-}
-
-export default App;
-///////////////////////////////////////////////////////////////////////////
-// import React from 'react';
-// import {RowColForm} from "./Components/RowColForm"
-
+// import './App.css';
+// import  { CarouselParent } from './Components/CarouselParent';
 // function App () {
 //     return(
 //         <div>
-//             <RowColForm />
-
+//             <CarouselParent/>
 //         </div>
-//     );
+//     )
+// }
+
+// export default App;
+///////////////////////////////////////////////////////////////////////////
+import React from 'react';
+import {RowColForm} from "./Components/RowColForm"
+
+function App () {
+    return(
+        <div>
+            <RowColForm />
+
+        </div>
+    );
 
     
-// };
+};
             
-// export default App;
+export default App;
--- myapp/src/Components/RowColForm.jsx
+++ myapp/src/Components/RowColForm.jsx
@@ -1,10 +1,101 @@
 import React from 'react';
-import { Row,Col } from 'react-bootstrap';
+import { Row,Col,Card, Form, Button } from 'react-bootstrap';
 
 export const RowColForm = () => {
     return (
         <div>
-            
+            <Row>
+                <Col md={2}></Col>
+                <Col md={4}>
+                    <Card>
+                        <Row>
+                            <Col>
+                            <Form>
+                                <Form.Group>
+                                    <Row>
+                                        <Col>
+                                        <Form.Control
+                                        type="text"
+                                        placehoder="First Name">
+
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                        <Form.Control
+                                        type="text"
+                                        placeholder="Last Name">
+
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                        <Form.label>Address:</Form.label>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                        <Form.Control
+                                        type="text"
+                                        placeholder="line 1">
+
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                        <Form.Control
+                                        type="text"
+                                        placeholder="line 2">
+
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                        <Form.Control
+                                        type="text"
+                                        placeholder="City">
+
+                                        </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                            </Form>
+                             </Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Row>
+                        <Col>
+                        <Button>Submit</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <Card>
+                            <Row>
+                                <Col>Salman Shaikh</Col>
+                            </Row>
+                            <Row>
+                                <Col>H.No. 2345</Col>
+                            </Row>
+                            <Row>
+                                <Col>Degloor Naka</Col>
+                            </Row>
+                            <Row>
+                                <Col>Nanded</Col>
+                            </Row>
+                        </Card>
+                        </Col>
+                    </Row>
+
+                </Col>
+                <Col md ={2}></Col>
+            </Row>
             
         </div>
     );
