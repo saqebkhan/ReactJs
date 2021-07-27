5750188b78a8abd6cc326bb35f2cd07e96630a17
saqebkhan <saqebk619@gmail.com>
2021-07-27 19:13:08 +0530

    27jul2021

--- myapp/src/Components/RowCol.jsx
+++ myapp/src/Components/RowCol.jsx
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
-import { Row, Col, Card, Form, Button } from "react-bootstrap";
-import { employeeReducer } from "./EmployeeReducer";
+import { Row, Col, Card, Form, Button, Image } from "react-bootstrap";
+import { RowColReducer } from "../Reducer/RowColReducer";
+import "./RowCol.css";
 // import './css/RowCol.css'
 
 export const RowCol = () => {
-  const { firstname, lastname, lineNo1, lineNo2, cityName } =
-    employeeReducer.Data[0];
-  const [fname, setFname] = useState("firstname");
-  const [lname, setLname] = useState("lastname");
-  const [line1, setline1] = useState("lineNo1");
-  const [line2, setline2] = useState("lineNo2");
-  const [city, setCity] = useState("cityName");
+  const { firstname, lastname, lineNo1, lineNo2, cityName, stateName } =
+    RowColReducer.Data[0];
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [line1, setline1] = useState("");
+  const [line2, setline2] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [qualification, setQname] = useState("");
+  const [dob, setDob] = useState("");
+  const [bulbState, setBulbState] = useState("On");
 
   return (
     <div>
       <Row>
-        <Col md={2}></Col>
+        <Col md={1}></Col>
         <Col md={4}>
-          <Card>
+          <Card id={bulbState === "On" ? "abc" : "abcAcc"}>
             <Row>
               <Col>
                 <Form>
                   <Form.Group>
                     <Row>
+                      <Form.Label>First Name:</Form.Label>
                       <Col>
                         <Form.Control
                           type="text"
@@ -33,6 +39,7 @@ export const RowCol = () => {
                       </Col>
                     </Row>
                     <Row>
+                      <Form.Label>Last Name:</Form.Label>
                       <Col>
                         <Form.Control
                           type="text"
@@ -43,31 +50,59 @@ export const RowCol = () => {
                       </Col>
                     </Row>
                     <Row>
-                      <Col>
-                        <Form.Label>Address:</Form.Label>
-                      </Col>
-                    </Row>
-                    <Row>
+                      <Form.Label>Qualification:</Form.Label>
                       <Col>
                         <Form.Control
                           type="text"
-                          placeholder="line 1"
+                          placeholder="Qualification"
+                          onChange={(e) => setQname(e.target.value)}
+                          value={qualification}
+                        ></Form.Control>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Form.Label>DOB:</Form.Label>
+                      <Col>
+                        <Form.Control
+                          type="date"
+                          placeholder="Date Of Birth"
+                          onChange={(e) => setDob(e.target.value)}
+                          value={dob}
+                        ></Form.Control>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col>
+                        <Form.Label>
+                          <b>Address:</b>
+                        </Form.Label>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Form.Label>House No:</Form.Label>
+                      <Col>
+                        <Form.Control
+                          type="text"
+                          placeholder="House No."
                           onChange={(e) => setline1(e.target.value)}
                           value={line1}
                         ></Form.Control>
                       </Col>
                     </Row>
                     <Row>
+                      <Form.Label>Street:</Form.Label>
                       <Col>
                         <Form.Control
                           type="text"
-                          placeholder="line 2"
+                          placeholder="Street"
                           onChange={(e) => setline2(e.target.value)}
                           value={line2}
                         ></Form.Control>
                       </Col>
                     </Row>
                     <Row>
+                      <Form.Label>City:</Form.Label>
                       <Col>
                         <Form.Control
                           type="text"
@@ -77,40 +112,78 @@ export const RowCol = () => {
                         ></Form.Control>
                       </Col>
                     </Row>
+                    <Row>
+                      <Form.Label>State:</Form.Label>
+                      <Col>
+                        <Form.Control
+                          type="text"
+                          placeholder="State"
+                          onChange={(e) => setState(e.target.value)}
+                          value={state}
+                        ></Form.Control>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Button>Submit</Button>
+                      </Col>
+                    </Row>
                   </Form.Group>
                 </Form>
               </Col>
             </Row>
           </Card>
         </Col>
-        <Col md={4}>
+        <Col md={5}>
           <Row>
             <Col>
-              <Button>Submit</Button>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Card>
+              <Card id={bulbState === "On" ? "abcd" : "abcdAcc"}>
                 <Row>
                   <Col>
-                    <h4>{`${fname}${lname},`}</h4>
+                    <h5>{`Name : ${fname} ${lname}`}</h5>
                   </Col>
                 </Row>
                 <Row>
-                  <Col>{`${line1}`}</Col>
+                  <Col>
+                    <h5>{`Qualification : ${qualification} `}</h5>
+                  </Col>
                 </Row>
                 <Row>
-                  <Col>{`${line2}`}</Col>
+                  <Col>
+                    <h5>{`D-O-B: ${dob}`}</h5>
+                  </Col>
                 </Row>
                 <Row>
-                  <Col>{`${city};`}</Col>
+                  <p>
+                    <b>Adreess:</b>
+                  </p>
+                  {` House Number ${line1} ,`}
                 </Row>
+                
+
+                {`${line2},`}
+                
+
+                {`${city},`}
+                
+
+                {`${state}.`}
               </Card>
             </Col>
           </Row>
         </Col>
-        <Col md={2}></Col>
+        
+        <Col md={2}>
+        <Image
+        class="img"
+        src={`bulb${bulbState}.png`}
+        height="200px"
+        width="100px"
+        onClick={() =>
+          bulbState === "Off" ? setBulbState("On") : setBulbState("Off")
+        }>
+          </Image>
+          </Col>
       </Row>
     </div>
   );
