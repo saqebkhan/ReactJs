e498c704a199a9494453ab1ea74de9f2cc423497
saqebkhan <saqebk619@gmail.com>
2021-07-27 19:12:04 +0530

    27july2021

---
+++ myapp/src/Reducer/RowColReducer.js
@@ -0,0 +1,14 @@
+export const RowColReducer={
+    Data:[
+        {
+            firstname:"Ashir",
+            lastname:"syed",
+            lineNo1:"H.NO.123",
+            lineNo2:"Chetan Nagar",
+            cityName: "Nanded",
+            stateName:"Maharashtra",
+            qualification:"MCA",
+            dob:"07/06/2021"
+        }
+    ]
+}
