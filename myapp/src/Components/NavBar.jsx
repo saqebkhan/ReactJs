626c2c76dec424633125ece36019d866c22fc8d4
saqebkhan <saqebk619@gmail.com>
2021-07-28 22:02:07 +0530

    28jul2021

--- myapp/src/Components/NavBar.jsx
+++ myapp/src/Components/NavBar.jsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import '../Components/css/NavBar.css' 
+import React from "react";
+import "../Components/css/NavBar.css";
 
 export const NavBar = () => {
-    return (
-        <div>
-            <div class="header">
-        
+  return (
+    <div>
+      <div class="header">
         <div class="overlay"></div>
         <div class="container">
-            <nav>
-                <h1 class="brand"><a href="index.html"><span style={{color:"blue"}}><b>Pickup</b></span><span style={{color:"yellow"}}>Biz</span></a></h1>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">About</a></li>
-                   <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+          <nav>
+            <img src="./pickupbizzlogo.png" alt="" />
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
         </div>
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default NavBar;
