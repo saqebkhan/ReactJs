74b91c1c39bde3acf002df6bc13cbb0de23857d3
saqebkhan <saqebk619@gmail.com>
2021-07-28 22:00:15 +0530

    28jul2021

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import HeaderComponent from './Component/A_HeaderSection/HeaderComponent';
 import BodySection from './Component/B_BodySection/BodySection';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import EMIcalculator from './EMIcalculator';
+import Piano from './Piano';
 
 function App() {
   return (
 <div className="App">
-<HeaderComponent/>
-<BodySection/>
+{/* <HeaderComponent/>
+<BodySection/> */}
+{/* <EMIcalculator/> */}
+<Piano/>
 
 
 
