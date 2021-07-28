1b1ee49168b4e5db09e1360d581ca189905ca8aa
saqebkhan <saqebk619@gmail.com>
2021-07-28 22:01:01 +0530

    28jul2021

--- myapp/src/NewComp.jsx
+++ myapp/src/NewComp.jsx
@@ -3,36 +3,79 @@ import { Card, Button } from "react-bootstrap";
 
 export const NewComp = () => {
   const [colorName, setColorName] = useState("Orange");
+
+  const bluehandler = () => {
+    setColorName("blue");
+  };
   return (
     <div>
       <Card>
-          <Button variant="link" onClick={()=>  colorName==="blue" ? setColorName("purple") : setColorName("blue")}>Change Me</Button>
-        <h1><span style={{color:"blue"}}>Pickup</span><span style={{color:"yellow"}}>Biz</span></h1>
+        <Button
+          variant="link"
+          onClick={() =>
+            colorName === "blue" ? setColorName("purple") : setColorName("blue")
+          }
+        >
+          Change Me
+        </Button>
+        <h1>
+          <span style={{ color: "blue" }}>Pickup</span>
+          <span style={{ color: "yellow" }}>Biz</span>
+        </h1>
         <p>This is my content</p>
         <span style={{ backgroundColor: colorName }}>
-          <h1 style={{color:"White"}}>PDAC Courses</h1>
+          <h1 style={{ color: "White" }}>PDAC Courses</h1>
         </span>
-        </Card>
-        <br/>
-        
-        <center>
+      </Card>
+      <br />
+
+      <center>
         <br />
         <br />
-        <Button style={{width:"680px"}} variant="outline-info" onClick={()=>setColorName("blue")}>Blue</Button>
+        <Button
+          style={{ width: "680px" }}
+          variant="outline-info"
+          onClick={bluehandler}
+        >
+          Blue
+        </Button>
         <br />
         <br />
-        <Button style={{width:"500px"}} variant="outline-danger" onClick={()=>setColorName("red")}>Red</Button>
+        <Button
+          style={{ width: "500px" }}
+          variant="outline-danger"
+          onClick={() => setColorName("red")}
+        >
+          Red
+        </Button>
         <br />
         <br />
-        <Button style={{width:"350px"}} variant="outline-success" onClick={()=>setColorName("green")}>Green</Button>
+        <Button
+          style={{ width: "350px" }}
+          variant="outline-success"
+          onClick={() => setColorName("green")}
+        >
+          Green
+        </Button>
         <br />
         <br />
-        <Button style={{width:"500px"}} variant="outline-danger" onClick={()=>setColorName("orange")}>Orange</Button>
+        <Button
+          style={{ width: "500px" }}
+          variant="outline-danger"
+          onClick={() => setColorName("orange")}
+        >
+          Orange
+        </Button>
         <br />
         <br />
-        <Button style={{width:"680px"}} variant="outline-warning" onClick={()=>setColorName("yellow")}>Yellow</Button>
-        </center>
-      
+        <Button
+          style={{ width: "680px" }}
+          variant="outline-warning"
+          onClick={() => setColorName("yellow")}
+        >
+          Yellow
+        </Button>
+      </center>
     </div>
   );
 };
