1594786a69e30c1dd965b378d224fc1b4c139689
saqebkhan <saqebk619@gmail.com>
2021-07-30 18:26:49 +0530

    30jul2021

---
+++ myapp2/src/Reducer/EmployeeReducer.js
@@ -0,0 +1,90 @@
+export const EmployeeReducer = {
+    Data:[
+        {
+            
+            Name:"mohd. Arbaz",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Ali Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Wahaj Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Miraj",
+            Class:"5th",
+             City : "49",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Majid Ali",
+            Class:"2nd",
+             City : "24",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "16",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Awez Pathan",
+            Class:"UKG",
+             City : "44",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"Nursury",
+             City : "90",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"6th",
+             City : "42",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "84",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "76",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "87",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "37",
+            imgUrl:"pp5.jpg"
+        },
+        ]
+    }
