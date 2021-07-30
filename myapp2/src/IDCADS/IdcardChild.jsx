9bf58f0bf6b5d648b10efa9ca8e8ee42adcb5251
saqebkhan <saqebk619@gmail.com>
2021-07-30 18:26:22 +0530

    30jul2021

---
+++ myapp2/src/IDCADS/IdcardChild.jsx
@@ -0,0 +1,30 @@
+// import React from "react";
+// import { Card, Button,  } from "react-bootstrap";
+// import "./Idcard.css";
+// import { IdcardReducer } from "./Reducer/IdcardReducer";
+
+// export const Idcard = () => {
+//   return (
+//     <div>
+      
+//           <input type="text" className="searchinput" placeholder="Search" />
+//           <Card style={{ width: "28rem" }} classNamec="card1">
+//             <Card.Img variant="top" src="logo192.png" className="img1" />
+//             <Card.Body>
+//               <Card.Title>Name : Saqeb Khan</Card.Title>
+//               <Card.Text>
+//                 Address : Aurangabad
+//                 <br />
+//                 Qualification : B.tech
+//                 <br />
+//                 Age : 21
+//               </Card.Text>
+//               <Button variant="outline-primary">Details</Button>
+//             </Card.Body>
+//           </Card>
+         
+//     </div>
+//   );
+// };
+
+// export default Idcard;
