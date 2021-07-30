3809f159476a043b7b80f40e08d5f46033bc0ca7
saqebkhan <saqebk619@gmail.com>
2021-07-30 18:25:40 +0530

    30jul2021

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import HeaderComponent from './Component/A_HeaderSection/HeaderComponent';
-import BodySection from './Component/B_BodySection/BodySection';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import EMIcalculator from './EMIcalculator';
-import Piano from './Piano';
+// import HeaderComponent from './Component/A_HeaderSection/HeaderComponent';
+// import BodySection from './Component/B_BodySection/BodySection';
+// import EMIcalculator from './EMIcalculator';
+// import Piano from './Piano';
+// import {Row,Col, Card} from 'react-bootstrap'
+// import AudioPlayer from './AudioPlayer';
+// import {Idcard} from './Idcard'
+import { EmployeeList} from './IDCADS/EmployeeList' 
 
 function App() {
   return (
 <div className="App">
-{/* <HeaderComponent/>
+{/* <HeaderComponent/> 
 <BodySection/> */}
 {/* <EMIcalculator/> */}
-<Piano/>
+{/* <Row><Col md={4}><Card><h1>Low</h1><Piano/></Card></Col>
+<Col md={4}><Card><h1>Medium</h1><Piano/></Card></Col>
+<Col md={4}><Card><h1>High</h1><Piano/></Card></Col></Row>
+<br/>
+<AudioPlayer/> */}
+{/* <Idcard/> */}
+<EmployeeList/>
 
 
 
