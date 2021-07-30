1cad0d87b445815b274f6dea19662026cec80df0
saqebkhan <saqebk619@gmail.com>
2021-07-30 18:25:50 +0530

    30jul2021

---
+++ myapp2/src/AudioPlayer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, Row, Col, Card } from "react-bootstrap";
+import "./Piano.css";
+
+export const AudioPlayer = () => {
+  const audio = new Audio("Money Heist Ringtone _Music Manda(MP3_70K).mp3");
+  const handleAudioPlay = () => {
+    audio.play();
+  };
+  const handleAudioPause = () => {
+    audio.pause();
+  };
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "a" && handleAudioPlay("A"));
+    if (e.key === "s" && handleAudioPlay("B"));
+    if (e.key === "d" && handleAudioPlay("D"));
+    if (e.key === "f" && handleAudioPlay("E"));
+    if (e.key === "g" && handleAudioPlay("F"));
+    if (e.key === "h" && handleAudioPlay("G"));
+  });
+  return (
+    <Card className="player">
+      <Row>
+        <Col></Col>
+        <Col>
+          <Button className={"playbtn"} onClick={handleAudioPlay}>
+            Play
+          </Button>
+        </Col>
+        <Col>
+          <Button className={"Pausebtn"} onClick={handleAudioPause}>
+            Pause
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            className={"thirdbtn"}
+            onMouseDown={handleAudioPlay}
+            onMouseUp
+          >
+            Piano
+          </Button>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default AudioPlayer;
