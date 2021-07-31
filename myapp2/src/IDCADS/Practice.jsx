d4570dc9a3de3251613da1c3022d3aa42f59b5d8
saqebkhan <saqebk619@gmail.com>
2021-08-01 00:00:22 +0530

    Create Practice.jsx

---
+++ myapp2/src/IDCADS/Practice.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+export const Practice = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>click to {count}</p>
+      <Button onClick={() => setCount(count + 2)}>Table 2</Button>Space
+      <Button onClick={() => setCount(count - 1)}>Minus</Button>
+    </div>
+  );
+};
+
+export default Practice;
