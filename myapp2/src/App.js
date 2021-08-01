7d12015e0264e9e6c52e9fc4e4dff7251ff1d4a9
saqebkhan <saqebk619@gmail.com>
2021-08-02 00:52:29 +0530

    Update App.js

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -2,29 +2,34 @@ import React from 'react';
 // import HeaderComponent from './Component/A_HeaderSection/HeaderComponent';
 // import BodySection from './Component/B_BodySection/BodySection';
 // import EMIcalculator from './EMIcalculator';
-// import Piano from './Piano';
+import Piano from './Piano';
 // import {Row,Col, Card} from 'react-bootstrap'
 // import AudioPlayer from './AudioPlayer';
 // import {Idcard} from './Idcard'
 // import { EmployeeList} from './IDCADS/EmployeeList' 
-import {Practice} from './IDCADS/Practice'
+// import {Practice} from './IDCADS/Practice'
+// import SectionArticle from './Component/SectionArticle'
 
 function App() {
   return (
 <div className="App">
 {/* <HeaderComponent/> 
 <BodySection/> */}
-{/* <EMIcalculator/> */}
-{/* <Row><Col md={4}><Card><h1>Low</h1><Piano/></Card></Col>
+{/* <EMIcalculator/>
+<Row><Col md={4}><Card><h1>Low</h1><Piano/></Card></Col>
 <Col md={4}><Card><h1>Medium</h1><Piano/></Card></Col>
 <Col md={4}><Card><h1>High</h1><Piano/></Card></Col></Row>
-<br/>
-<AudioPlayer/> */}
+<br/> */}
+<AudioPlayer/>
+
 {/* <Idcard/> */}
 {/* <EmployeeList/> */}
-<Practice/>
+{/* <Practice/> */}
+{/* <SectionArticle/> */}
 
 </div>
   );
 };
 export default App;
+
+
