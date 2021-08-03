59ffc6c854fe53f5c70d129ea74e5f8b51de6e59
saqebkhan <saqebk619@gmail.com>
2021-08-03 23:02:01 +0530

    Update EmployeeList.jsx

--- myapp2/src/IDCADS/EmployeeList.jsx
+++ myapp2/src/IDCADS/EmployeeList.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Card, Col, Row, Form } from "react-bootstrap";
+import { useHistory } from "react-router";
 import "./Employee.css";
 import { EmployeeItem } from "./EmployeItem";
 import { EmployeeReducer } from "../Reducer/EmployeeReducer";
+// import {userData} from"../config"
 
 export const EmployeeList = () => {
   const [empArr, setEmpArr] = useState(EmployeeReducer.Data);
   const [searchText, setSearchText] = useState(" ");
+  const history = useHistory
   useEffect (() => {
     const filterArr = empArr.filter(
       (item) =>
         item.Name.toLowerCase().includes(searchText.toLowerCase()) ||
-        item.City.toLowerCase().includes(searchText.toLowerCase())
+        item.City.toLowerCase().includes(searchText.toLowerCase()) 
     );
     if (searchText === "") setEmpArr(EmployeeReducer.Data);
     else setEmpArr(filterArr);
   }, [searchText])
+
   return (
     <Card className="employeelist">
       <h2>Employee List</h2>
@@ -36,3 +40,4 @@ export const EmployeeList = () => {
   );
 };
 
+export default EmployeeList;
