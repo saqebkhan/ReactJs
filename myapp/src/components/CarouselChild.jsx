201be11591a52f47e6d7cf1f467f5848f25c126b
saqebkhan <saqebk619@gmail.com>
2021-08-04 16:17:30 +0530

    Update CarouselChild.jsx

--- myapp/src/components/CarouselChild.jsx
+++ myapp/src/components/CarouselChild.jsx
@@ -1,58 +1,58 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
-import "./Carousel.css";
+// import React from "react";
+// import { Carousel } from "react-bootstrap";
+// import "./Carousel.css";
 
-export const CarouselChild = ({ item }) => {
-  return (
-    <div style={{ height: "600px" }}>
-      <center>
-      <Carousel className="abc">
+// export const CarouselChild = ({ item }) => {
+//   return (
+//     <div style={{ height: "600px" }}>
+//       <center>
+//       <Carousel className="abc">
         
-        <Carousel.Item interval={4000}>
-          <img
-            className="imgs"
-            src="https://apexvisas.ae/wp-content/uploads/2018/08/1.jpg"
-            alt="First slide"
-            style={{ height: "500px" }}
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>
-              Nulla vitae elit libero, a pharetra augue mollis interdum.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={3500}>
-          <img
-            className="imgs"
-            src="https://www.studyfinds.org/wp-content/uploads/2018/08/adult-american-arms-1265902.jpg"
-            alt="Second slide"
-            style={{ height: "500px" }}
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className="imgs"
-            src="https://buffer.com/resources/content/images/resources/wp-content/uploads/2018/11/free-images-featured.png"
-            alt="Third slide"
-            style={{ height: "500px" }}
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      </center>
-    </div>
-  );
-};
+//         <Carousel.Item interval={4000}>
+//           <img
+//             className="imgs"
+//             src="https://apexvisas.ae/wp-content/uploads/2018/08/1.jpg"
+//             alt="First slide"
+//             style={{ height: "500px" }}
+//           />
+//           <Carousel.Caption>
+//             <h3>First slide label</h3>
+//             <p>
+//               Nulla vitae elit libero, a pharetra augue mollis interdum.
+//             </p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//         <Carousel.Item interval={3500}>
+//           <img
+//             className="imgs"
+//             src="https://www.studyfinds.org/wp-content/uploads/2018/08/adult-american-arms-1265902.jpg"
+//             alt="Second slide"
+//             style={{ height: "500px" }}
+//           />
+//           <Carousel.Caption>
+//             <h3>Second slide label</h3>
+//             <p>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+//             </p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//         <Carousel.Item interval={3000}>
+//           <img
+//             className="imgs"
+//             src="https://buffer.com/resources/content/images/resources/wp-content/uploads/2018/11/free-images-featured.png"
+//             alt="Third slide"
+//             style={{ height: "500px" }}
+//           />
+//           <Carousel.Caption>
+//             <h3>Third slide label</h3>
+//             <p>
+//               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//             </p>
+//           </Carousel.Caption>
+//         </Carousel.Item>
+//       </Carousel>
+//       </center>
+//     </div>
+//   );
+// };
 
