a1eb0bbbf8c141a1d1f03a8450671ed277e5731f
saqebkhan <saqebk619@gmail.com>
2021-08-04 16:17:55 +0530

    Update CarouselParent.jsx

--- myapp/src/components/CarouselParent.jsx
+++ myapp/src/components/CarouselParent.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { CarouselChild } from "./CarouselChild";
-import { CarouselReducer } from "../Reducer/CarouselReducer";
-export const CarouselParent = () => {
-  console.log(CarouselReducer);
+// import React from "react";
+// import { CarouselChild } from "./CarouselChild";
+// import { CarouselReducer } from "../Reducer/CarouselReducer";
+// export const CarouselParent = () => {
+//   console.log(CarouselReducer);
 
-  return (
-    <div>
-      {CarouselReducer.Data.map((item) => {
-        return <CarouselChild kaimebhi={item} />;
-      })}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {CarouselReducer.Data.map((item) => {
+//         return <CarouselChild kaimebhi={item} />;
+//       })}
+//     </div>
+//   );
+// };
 
