5efbc2b2f6101811bd59db5da180c742c394e986
saqebkhan <saqebk619@gmail.com>
2021-08-06 23:21:30 +0530

    Create RightContent.jsx

---
+++ myapp/src/Dictionary/RightContent.jsx
@@ -0,0 +1,30 @@
+import React from'react'
+import {Card,Button,Dropdown} from 'react-bootstrap'
+
+export const RightContent=()=>{
+    return(
+        <div>
+             <Card className="RightContent">
+
+          <p>Right Content here</p>
+          <Button>English</Button>
+      </Card>
+      <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Language (English)
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">English UK</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">English US</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+
+        </div>
+
+
+
+    )
+}
+export default RightContent;
