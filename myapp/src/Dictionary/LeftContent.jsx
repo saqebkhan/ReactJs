4381a07b001f219f929c8cde9b6c1c56b79d10b8
saqebkhan <saqebk619@gmail.com>
2021-08-09 16:37:47 +0530

    Update LeftContent.jsx

--- myapp/src/Dictionary/LeftContent.jsx
+++ myapp/src/Dictionary/LeftContent.jsx
@@ -3,27 +3,30 @@ import { Card, Form, Row, Col, Button } from "react-bootstrap";
 import Meaning from "./Meaning";
 // import Meaning from './Meaning'
 
-export const LeftContent = ({ hanlesearch,setWordSearch,handleKeyPress }) => {
+export const LeftContent = ({
+  handlesearch,
+  setWordSearch,
+}) => {
   return (
     <div>
-      <Card className="pronounciation"> 
+      <Card className="pronounciation">
         <Row>
           <Col md={10}>
             <Form.Control
               type="text"
               placeholder="Search here"
               onChange={(e) => setWordSearch(e.target.value)}
+              onKeyPress={handlesearch}
             ></Form.Control>
             <span>Pronouncination</span>
             <Button>Play</Button>
           </Col>
           <Col md={2}>
-            <Button onClick={hanlesearch} onKeyPress={handleKeyPress}>Search</Button>
+            <Button onClick={handlesearch}>Search</Button>
           </Col>
         </Row>
 
         <span>Pronoun</span>
-        
       </Card>
     </div>
   );
