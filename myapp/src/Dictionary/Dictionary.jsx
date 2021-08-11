2d44d1c770e2c8d0da8c532f2eae7ca5d1dbb78a
saqebkhan <saqebk619@gmail.com>
2021-08-11 17:02:22 +0530

    Update Dictionary.jsx

--- myapp/src/Dictionary/Dictionary.jsx
+++ myapp/src/Dictionary/Dictionary.jsx
@@ -6,23 +6,23 @@ import LeftContent from "./LeftContent";
 import RightContent from "./RightContent";
 import MeaningList from "./MeaningList";
 import axios from "axios";
+import Synonyms from "./Synnonyms/Synonyms";
 
 const Dictionary = () => {
   const [resData, setResData] = useState([]);
   const [wordSearch, setWordSearch] = useState(" ");
 
   const handlesearch = (e) => {
-    if(e.code === "Enter" || e.type === "click") {
-    axios
-      .get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${wordSearch}`)
-      .then((res) => {
-        console.log(res.data);
-        setResData([...res.data]);
-      });
+    if (e.code === "Enter" || e.type === "click") {
+      axios
+        .get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${wordSearch}`)
+        .then((res) => {
+          console.log(res.data);
+          setResData([...res.data]);
+        });
     }
   };
-
-
+  
 
   return (
     <div>
@@ -40,8 +40,6 @@ const Dictionary = () => {
               (item) => item.meanings.length > 0 && <MeaningList item={item} />
             )}
 
-            {/* <MeaningList />
-            <MeaningList /> */}
           </Col>
           <Col md={4}>
             <RightContent />
