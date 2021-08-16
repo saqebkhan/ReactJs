ce7d7b5375c9c0c15af99d265f381fa2f4d93a4a
saqebkhan <saqebk619@gmail.com>
2021-08-16 21:11:45 +0530

    Create EmployeeReducer.js

---
+++ myapp2/src/Sessions Practice/A_HeaderSection/Reducer/EmployeeReducer.js
@@ -0,0 +1,735 @@
+export const EmployeeReducer = {
+    Data:[
+        {
+            
+            Name:"mohd. Arbaz",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Ali Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Wahaj Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Miraj",
+            Class:"5th",
+             City : "Ahemdabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Majid Ali",
+            Class:"2nd",
+             City : "Banglore",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "Hydrabad",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Awez Pathan",
+            Class:"UKG",
+             City : "Nanded",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"Nursury",
+             City : "Jalna",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"6th",
+             City : "Akola",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "Ahemad Nagar",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "Pune",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "Mumbai",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"Sameer Pathan",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Abuzar Wahid",
+            Class:"8th",
+             City : "Delhi",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Kalim ansari",
+            Class:"9th",
+             City : "Agra",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Muzammil Shaikh",
+            Class:"7th",
+             City : "VishakhaPatnam",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"ilyas Khan",
+            Class:"5th",
+             City : "Lakhnow",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Mohd. Maaz",
+            Class:"1st",
+             City : "Jhansi",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Mazhar Khatib",
+            Class:"1st",
+             City : "BhopaL",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Arbaz Altamash",
+            Class:"1st",
+             City : "Nagpur",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"qazi Zuber",
+            Class:"3rg",
+             City : "Solapur",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"adity kodi",
+            Class:"3rd",
+             City : "Kolhapur",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            
+            Name:"mohd. Arbaz",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Ali Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Wahaj Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Miraj",
+            Class:"5th",
+             City : "Ahemdabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Majid Ali",
+            Class:"2nd",
+             City : "Banglore",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "Hydrabad",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Awez Pathan",
+            Class:"UKG",
+             City : "Nanded",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"Nursury",
+             City : "Jalna",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"6th",
+             City : "Akola",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "Ahemad Nagar",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "Pune",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "Mumbai",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"Sameer Pathan",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Abuzar Wahid",
+            Class:"8th",
+             City : "Delhi",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Kalim ansari",
+            Class:"9th",
+             City : "Agra",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Muzammil Shaikh",
+            Class:"7th",
+             City : "VishakhaPatnam",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"ilyas Khan",
+            Class:"5th",
+             City : "Lakhnow",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Mohd. Maaz",
+            Class:"1st",
+             City : "Jhansi",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Mazhar Khatib",
+            Class:"1st",
+             City : "BhopaL",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Arbaz Altamash",
+            Class:"1st",
+             City : "Nagpur",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Umer Zuber",
+            Class:"3rg",
+             City : "Solapur",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"adity kodi",
+            Class:"3rd",
+             City : "Kolhapur",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            
+            Name:"mohd. Adnan",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Faizan Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Akif Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Kamran",
+            Class:"5th",
+             City : "Ahemdabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Shoeb Ali",
+            Class:"2nd",
+             City : "Banglore",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "Hydrabad",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Akhtar Pathan",
+            Class:"UKG",
+             City : "Nanded",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Rizwan",
+            Class:"Nursury",
+             City : "Jalna",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Siddiq Amer",
+            Class:"6th",
+             City : "Akola",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "Ahemad Nagar",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "Pune",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "Mumbai",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"Sameer Pathan",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Abuzar Wahid",
+            Class:"8th",
+             City : "Delhi",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Kalim ansari",
+            Class:"9th",
+             City : "Agra",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Muzammil Shaikh",
+            Class:"7th",
+             City : "VishakhaPatnam",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"ilyas Khan",
+            Class:"5th",
+             City : "Lakhnow",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Mohd. Maaz",
+            Class:"1st",
+             City : "Jhansi",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Mazhar Khatib",
+            Class:"1st",
+             City : "BhopaL",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Arbaz Altamash",
+            Class:"1st",
+             City : "Nagpur",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"qazi Zuber",
+            Class:"3rg",
+             City : "Solapur",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"adity kodi",
+            Class:"3rd",
+             City : "Kolhapur",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            
+            Name:"mohd. Arbaz",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Ali Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Wahaj Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Miraj",
+            Class:"5th",
+             City : "Ahemdabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Majid Ali",
+            Class:"2nd",
+             City : "Banglore",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "Hydrabad",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Awez Pathan",
+            Class:"UKG",
+             City : "Nanded",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"Nursury",
+             City : "Jalna",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"6th",
+             City : "Akola",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "Ahemad Nagar",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "Pune",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "Mumbai",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"Sameer Pathan",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Abuzar Wahid",
+            Class:"8th",
+             City : "Delhi",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Kalim ansari",
+            Class:"9th",
+             City : "Agra",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"Muzammil Shaikh",
+            Class:"7th",
+             City : "VishakhaPatnam",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"ilyas Khan",
+            Class:"5th",
+             City : "Lakhnow",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Mohd. Maaz",
+            Class:"1st",
+             City : "Jhansi",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Mazhar Khatib",
+            Class:"1st",
+             City : "BhopaL",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Arbaz Altamash",
+            Class:"1st",
+             City : "Nagpur",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"qazi Zuber",
+            Class:"3rg",
+             City : "Solapur",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"adity kodi",
+            Class:"3rd",
+             City : "Kolhapur",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            
+            Name:"mohd. Arbaz",
+            Class: "8th",
+             City : "Mumbai",
+            imgUrl: "Profile1.png"
+        },
+
+        {
+            Name:"Ali Khan",
+            Class:"5th",
+             City : "Pune",
+            imgUrl:"pp6.jpg"
+        },
+        {
+            Name:"momin Khan",
+            Class:"7th",
+             City : "Aurangabad",
+            imgUrl:"images (1).jpg"
+        },
+        {
+            Name:"Wahaj Shaikh",
+            Class:"5th",
+             City : "Nanded",
+            imgUrl:"images (2).jpg"
+        },
+        {
+            Name:"Adil Miraj",
+            Class:"5th",
+             City : "Ahemdabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Majid Ali",
+            Class:"2nd",
+             City : "Banglore",
+            imgUrl:"download.png"
+        },
+        {
+            Name:"Sameer Siddiquee",
+            Class:"LKG",
+             City : "Hydrabad",
+            imgUrl:"download (2).jpg"
+        },
+        {
+            Name:"Awez Pathan",
+            Class:"UKG",
+             City : "Nanded",
+            imgUrl:"download (1).jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"Nursury",
+             City : "Jalna",
+            imgUrl:"download.jpg"
+        },
+        {
+            Name:"Wali Ala",
+            Class:"6th",
+             City : "Akola",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Khan samee",
+            Class:"3rd",
+             City : "Ahemad Nagar",
+            imgUrl:"pp5.jpg"
+        },
+        {
+            Name:"Khan shehzad",
+            Class:"2nd",
+             City : "Pune",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Moin Shaikh",
+            Class:"1st",
+             City : "Mumbai",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Wasim syed",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp7.jpg"
+        },
+        {
+            Name:"Sameer Pathan",
+            Class:"1st",
+             City : "Aurangabad",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"Abuzar Wahid",
+            Class:"8th",
+             City : "Delhi",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Kalim ansari",
+            Class:"9th",
+             City : "Agra",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Muzammil Shaikh",
+            Class:"7th",
+             City : "VishakhaPatnam",
+            imgUrl:"pp2.jpg"
+        },
+        {
+            Name:"ilyas Khan",
+            Class:"5th",
+             City : "Lakhnow",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"Mohd. Maaz",
+            Class:"1st",
+             City : "Jhansi",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"Mazhar Khatib",
+            Class:"1st",
+             City : "BhopaL",
+            imgUrl:"pp4.jpg"
+        },
+        {
+            Name:"Arbaz Altamash",
+            Class:"1st",
+             City : "Nagpur",
+            imgUrl:"pp1.jpg"
+        },
+        {
+            Name:"qazi Zuber",
+            Class:"3rg",
+             City : "Solapur",
+            imgUrl:"pp3.jpg"
+        },
+        {
+            Name:"adity kodi",
+            Class:"3rd",
+             City : "Kolhapur",
+            imgUrl:"pp7.jpg"
+        },
+        ]
+    }
+
