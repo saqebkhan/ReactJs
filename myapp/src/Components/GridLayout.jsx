9a176e0f37a5951f3cf13000678b71edced93a28
saqebkhan <saqebk619@gmail.com>
2021-08-16 21:09:13 +0530

    Update GridLayout.jsx

--- myapp/src/Components/GridLayout.jsx
+++ myapp/src/Components/GridLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
-import "./css/GridLayout.css";
+import "GridLayout.css"
 
 export const GridLayout = () => {
   return (
