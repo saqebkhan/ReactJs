04ba9413cab27eba5f6243b3d91b54a4f67ef9e0
saqebkhan <saqebk619@gmail.com>
2021-08-16 21:12:01 +0530

    Create EmployeeList.jsx

---
+++ myapp2/src/Sessions Practice/EmployeeList.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Card, Col, Row, Form } from "react-bootstrap";
+import { useHistory } from "react-router";
+import "./Employee.css";
+import { EmployeeItem } from "./EmployeItem";
+import { EmployeeReducer } from "./A_HeaderSection/Reducer/EmployeeReducer";
+// import {userData} from"../config"
+
+export const EmployeeList = () => {
+  const [empArr, setEmpArr] = useState(EmployeeReducer.Data);
+  const [searchText, setSearchText] = useState(" ");
+  const history = useHistory;
+  useEffect(() => {
+    const filterArr = empArr.filter(
+      (item) =>
+        item.Name.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.City.toLowerCase().includes(searchText.toLowerCase())
+    );
+    if (searchText == "") setEmpArr(EmployeeReducer.Data);
+    else setEmpArr(filterArr);
+  }, [searchText]);
+
+  return (
+    <Card className="employeelist">
+      <h2>Employee List</h2>
+      <Form>
+        <Form.Control
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          placeholder="Search"
+        ></Form.Control>
+      </Form>
+      <Row>
+        {empArr.map((item) => (
+          <Col md={3}>
+            <EmployeeItem itemData={item} />
+          </Col>
+        ))}
+      </Row>
+    </Card>
+  );
+};
+
+export default EmployeeList;
