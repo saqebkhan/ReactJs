c60c4e4999ffd4a5a032d094a944baa58094a986
saqebkhan <saqebk619@gmail.com>
2021-08-19 03:07:56 +0530

    Create ToDoItem.jsx

---
+++ myapp2/src/Sessions Practice/ToDoList/ToDoItem.jsx
@@ -0,0 +1,27 @@
+import { React, useEffect, useState } from "react";
+import { Card, Row, Col, Form, Image } from "react-bootstrap";
+
+export const ToDoItem = ({ item, todoitems }) => {
+  const [check, setCheck] = useState(false);
+  // if (todoitems.indexOf("Completed All") === check) {
+  //   todoitems.forEach((item) => {
+  //     item = check;
+  //   });
+  // }
+  
+  return (
+    <div>
+      <Image className="imgtdi" />
+      <Card className="cardtdi">
+        <Row>
+          <Col md={1}>
+            <Form.Check onClick={() => setCheck(!check)}></Form.Check>
+          </Col>
+          <Col md={11}>
+            <label htmlFor="">{check ? <del>{item}</del> : item}</label>
+          </Col>
+        </Row>
+      </Card>
+    </div>
+  );
+};
