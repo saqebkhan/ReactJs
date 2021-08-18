2107f563582c3c16af1d36774781b36596701a4a
saqebkhan <saqebk619@gmail.com>
2021-08-18 16:22:38 +0530

    Create reducer.js

---
+++ myapp2/src/Router/Practice/reducer.js
@@ -0,0 +1,20 @@
+export const cardDetails={
+    info:[
+    {
+        Title:"Good",
+        Img:"logo192.png",
+        Text:"Behaviour"
+    },
+    {
+        Title:"Good",
+        Img:"logo192.png",
+        Text:"Behaviour"
+    },
+    {
+        Title:"Good",
+        Img:"logo192.png",
+        Text:"Behaviour"
+    },
+]}
+
+
