7a6a909cf84777c1cffe067b1eda20aefb399da3
saqebkhan <saqebk619@gmail.com>
2021-09-12 13:10:19 +0530

    Update App.js

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+// import ParentComponent from "./Practice/ParentComponent"
+// import DragNDrop from "./TodoList/DragNDrop";
+import DragNDrop from "./TodoList/DragNDrop";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ParentComponent/> */}
+      <DragNDrop />
     </div>
   );
-}
+};
 
 export default App;
